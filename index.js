4a65512b580ce7779ddc083035b49b6c7fd51aec
Manish Kumar <mmm.manish163@gmail.com>
2023-05-05 07:52:27 +0530

    basic mongoDB code

---
+++ index.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/Sample", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//schema
+const student = new mongoose.Schema({
+  name: String,
+  workout: Boolean,
+  height: Number,
+});
+
+//models ->object
+const Student = new mongoose.model("Student", student);
+
+const adder = async () => {
+  const ss = await Student.create({
+    name: "prateek",
+    workout: false,
+    height: 5,
+  });
+  console.log(ss);
+};
+adder();
